c3ef22906cb1d681f7b35f5b6a9a696405691e56
Ryan Olson <ryan.olson86@yahoo.com>
2024-02-06 11:50:03 -0600

    Added conditional rendering to AddEvent on EventsPage

--- src/components/EventsPage/EventsPage.jsx
+++ src/components/EventsPage/EventsPage.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import UpcomingEvents from "../UpcomingEvents/UpcomingEvents";
 import AddEvent from "../AddEvent/AddEvent";
+import { useSelector } from "react-redux";
 
 function EventsPage() {
+  const user = useSelector(store => store.user)
+
   return (
     <>
-      <AddEvent />
+      {user.access_level === 1 && <AddEvent />}
       <UpcomingEvents />
     </>
   );
