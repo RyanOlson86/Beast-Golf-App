de66a1c92f8f6c7e271353fb3d5d544d8a9c5d38
Ryan Olson <ryan.olson86@yahoo.com>
2024-02-06 10:21:04 -0600

    Set up events reducer and event saga for GET

---
+++ src/redux/sagas/events.saga.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import { put, takeLatest } from 'redux-saga/effects';
+
+// Function to GET events from server, then send to events reducer
+function* fetchEvents() {
+  try {
+    const response = yield axios.get('/api/events');
+    yield put({ type: 'SET_EVENTS', payload: response.data });
+  } catch (error) {
+    console.log('Events get request failed', error);
+  }
+}
+
+function* eventsSaga() {
+  yield takeLatest('FETCH_USER', fetchEvents);
+}
+
+export default eventsSaga;
