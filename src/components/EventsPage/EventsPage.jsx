b1fe2972657504c06f213b06013d98da084cb305
Ryan Olson <ryan.olson86@yahoo.com>
2024-02-06 11:58:20 -0600

    Added useEffect on EventsPage. Changed LogOutButton to change page to home

--- src/components/EventsPage/EventsPage.jsx
+++ src/components/EventsPage/EventsPage.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import UpcomingEvents from "../UpcomingEvents/UpcomingEvents";
 import AddEvent from "../AddEvent/AddEvent";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 function EventsPage() {
   const user = useSelector(store => store.user)
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    dispatch({type: 'FETCH_EVENTS'})
+  },[])
 
   return (
     <>
