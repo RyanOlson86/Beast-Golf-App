711a92fed8687087ec0113684b8f53b935d92e3f
Ryan Olson <ryan.olson86@yahoo.com>
2024-02-06 15:34:31 -0600

    Added ModifyEvents page and displayed event details from useParams

--- src/components/ModifyEvents/ModifyEvents.jsx
+++ src/components/ModifyEvents/ModifyEvents.jsx
@@ -1,18 +1,47 @@
 import React, { useEffect } from "react";
-import UpcomingEvents from "../UpcomingEvents/UpcomingEvents";
-import AddEvent from "../AddEvent/AddEvent";
 import { useSelector, useDispatch } from "react-redux";
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { Box, Grid, Typography } from "@mui/material";
 
 function ModifyEvents() {
-  const user = useSelector(store => store.user)
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch({ type: "FETCH_EVENTS" });
+  }, []);
+
+  // Fetch all events from store
+  const events = useSelector((store) => store.events);
+
+  // Get event ID from useParams
+  const { id } = useParams();
+
+  // Filter All events to specific event by ID
+  const eventArray = events.filter((event) => event?.id == id);
+  const eventDetails = eventArray[0];
 
   return (
-    <>
-      {/* {user.access_level === 1 && <AddEvent />}
-      <UpcomingEvents /> */}
-      <h2>Modify events</h2>
-    </>
+    <Box sx={{ flexGrow: 1 , textAlign: 'center'}}>
+      <Grid container spacing={2}>
+        <Grid item xs={3}>
+          <Typography variant="h6">Course:</Typography >
+          <Typography variant="h6">{eventDetails?.course}</Typography>
+        </Grid>
+        <Grid item xs={3}>
+          <Typography variant="h6">Format:</Typography>
+          <Typography variant="h6">{eventDetails?.format}</Typography>
+        </Grid>
+        <Grid item xs={3}>
+          <Typography variant="h6">Tee Box:</Typography>
+          <Typography variant="h6">{eventDetails?.teebox}</Typography>
+        </Grid>
+        <Grid item xs={3}>
+          <Typography variant="h6">Date:</Typography>
+          <Typography variant="h6">{new Date(eventDetails?.date).toLocaleDateString()}</Typography>
+        </Grid>
+      </Grid>
+      {/* Add PlayerList component and pass event */}
+    </Box>
   );
 }
 
