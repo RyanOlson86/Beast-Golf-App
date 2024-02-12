38526f7a70b574035113e6db415e8f3038ad1d4b
Ryan Olson <ryan.olson86@yahoo.com>
2024-02-12 15:56:30 -0600

    Added user stats to the Leaderboard page if the user is logged in

--- src/components/Leaderboard/Leaderboard.jsx
+++ src/components/Leaderboard/Leaderboard.jsx
@@ -30,16 +30,27 @@ const columns = [
 
 function Leaderboard() {
   const players = useSelector((store) => store.players);
-  const [open, setOpen] = useState(false);
-  const [playerDetails, setPlayerDetails] = useState([]);
-  const [playerName, setPlayerName] = useState("");
-  const dispatch = useDispatch();
+  const user = useSelector((store) => store.user);
 
+  // Local State for Modal control
+  const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
+  // Local State for player details selected from Leaderboard
+  const [playerDetails, setPlayerDetails] = useState([]);
+  const [playerName, setPlayerName] = useState("");
+
+  // Local State for UserDetails
+  const userDetails = players.filter(player => player.id === user.player_id)
+
+  const dispatch = useDispatch();
+
   useEffect(() => {
     dispatch({ type: "FETCH_ALL_PLAYERS" });
+    if (user.player_id != null) {
+      fetchDetails(user.player_id, userDetails[0].full_name);
+    }
   }, []);
 
   const fetchDetails = (id, name) => {
@@ -47,25 +58,30 @@ function Leaderboard() {
       .get(`api/details/${id}`)
       .then((response) => {
         setPlayerDetails(response.data);
-        setPlayerName(name);
+        if (name != undefined) {
+          setPlayerName(name);
+        }
       })
       .catch((error) => {
         console.log("Error in fetchDetails", error);
       });
   };
 
-  // Local state that is updated when a row is clicked on event list
-  const [rowId, setRowId] = useState(0);
-
   return (
     <Box sx={{ height: 600, width: "90%", m: "5%" }}>
+      {user.player_id && (
+        <Box sx={{marginBottom: '5%'}}>
+          <Typography variant="h4" >Welcome {userDetails[0].full_name}!</Typography>
+          <Typography variant="h6" >You have played <b>{userDetails[0].events_played}</b> events with <b>{userDetails[0].wins}</b> wins!</Typography>
+        </Box>
+      )}
       <Typography variant="h5">Leaderboard:</Typography>
       <DataGrid
         rows={players}
         columns={columns}
         initialState={{
           sorting: {
-            sortModel: [{ field: 'events_played', sort: 'desc' }],
+            sortModel: [{ field: "events_played", sort: "desc" }],
           },
         }}
         onRowClick={(params) => {
