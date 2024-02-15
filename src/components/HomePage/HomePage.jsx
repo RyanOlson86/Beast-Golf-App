490722899661e433e8f942137867a7858326dec3
Ryan Olson <ryan.olson86@yahoo.com>
2024-02-15 15:03:18 -0600

    started adding background

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -9,17 +9,17 @@ function HomePage() {
   return (
     <>
       <center>
-        <Box sx={{ display: "flex", width: "90%", textAlign: 'justify', marginBottom: 10 }}>
-          <Box sx={{ width: "60%", marginRight: 5 }}>
+        <Box sx={{ display: "flex", width: "90%", textAlign: 'justify', marginBottom: 10, bgcolor: 'white', borderRadius: 2 }}>
+          <Box sx={{ width: "60%", p: 3 }}>
             <Typography variant="h4">Welcome to the BGA!</Typography>
             <Typography variant="p">
               The Beast Golf Association (BGA) was started in 2019 when a group of friends decided to form their own golf league,
               playing 2-person scramble format to promote a level playing field while promoting comradery and competition amongst
               the group. The BGA quickly grew as others friends and family members joined the weekly rounds of golf and now over
-              200 different players have played in the BGA.
+              230 different players have played in the BGA in over 100 events!
             </Typography>
           </Box>
-          <Box sx={{ maxWidth: "600px" }}>
+          <Box sx={{ maxWidth: "600px", p: 3 }}>
             <img src="public/images/Screenshot 2024-02-15 at 10.47.24 AM.png" />
           </Box>
         </Box>
